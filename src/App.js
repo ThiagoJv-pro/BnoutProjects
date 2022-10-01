c910b95667e895c0bf79686091d9997745f5de97
Thiago de Jesus Vieira <thijv7@gmail.com>
2022-10-01 16:30:42 -0300

    core

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import logo from './imgs/logoPngsooB.png';
-import imgBody from './imgs/lv3.png';
+import engr from './imgs/lv3.png';
 
 function App() {
   return (
@@ -8,14 +8,15 @@ function App() {
         <div class="logo">
           <img id="logoImg" src={logo} alt="logo"/>
         </div>
-        
-        <div id='animation'>
-          <img id="logoImg2" src={imgBody}/>
+
+        <div class="animation">
+          <img id="logoImg2" src={engr} alt="engrenagem"/>
         </div>
 
         <div class="text">
         <h1>Under development...</h1>
         </div>
+
       </body>
     
   );
