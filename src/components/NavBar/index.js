df2e8aeb4cc6d12eea7f25fc613c6605821e6b02
Thiago de Jesus Vieira <thijv7@gmail.com>
2022-10-17 10:06:05 -0300

    logo

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from './imgs/logoPngsooB.png';
+import './style.css';
 
 function NavBar() {
     return (
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a className='navbar-brand' href='#'>
+                <img src={Logo} id="imgLogo"/>
+                Bnout Tech
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
