d368f3810f5475516383ade2cf3a9b90e1c4c7e9
Thiago de Jesus Vieira <thijv7@gmail.com>
2022-11-28 20:04:45 -0300

    teste

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,10 +6,6 @@ function NavBar() {
     return (
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a className='navbar-brand' href='#'>
-                <img src={Logo} id="imgLogo"/>
-                Bnout Techs
-            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
