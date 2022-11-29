74e17ddbebe922e0231a914bafd8801f55217455
Thiago de Jesus Vieira <thijv7@gmail.com>
2022-11-29 20:27:39 -0300

    teste

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,7 +7,19 @@ function NavBar() {
     return (
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Services</a>
+                    </li>
+                </ul>
+            </div>
             <div class="logo">
                  <img id="logoImg" src={logo} alt="logo"/>
             </div>
